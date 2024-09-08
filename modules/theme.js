50469700f86643287756ad61cdaf957c077f7f39
Damian Ponce <dponcez_git@hotmail.com>
2024-09-07 21:22:47 -0500

    feat: add local storage to store user preference when selecting a mode (light or dark)

--- modules/theme.js
+++ modules/theme.js
@@ -1,23 +1,34 @@
 import { selector } from "./custom_functions.js";
 import { eventHandler } from "./custom_functions.js";
 
-const switchThemeButton = selector('[data-switch]');
+const themeButton = selector('[data-toggle]');
 const container = selector('.container');
 
-export const handleDarkMode = () => {
-  eventHandler(switchThemeButton, 'click', (event) => {
-    const suffix = event.target;
-    const dark = '../assets/images/small-dark-switch.png';
-    const light = '../assets/images/small-light-switch.png';
+export const handleDarkMode = () => {  
+  eventHandler(themeButton, 'click', () => {
+    
+    container.classList.toggle('dark');
+    themeButton.classList.toggle('active');
 
-    if(suffix.classList.contains('dark')){
-      suffix.classList.remove('dark');
-      container.setAttribute('id', 'dark')
-      suffix.src= `${dark}`;
+    const storage = (key, value) => localStorage.setItem(key, value);
+    
+    container.classList.contains('dark') ?
+      storage('theme', 'true') :
+      storage('theme', 'false');
+  });
+
+  const darkMode = (key, value) => {
+    const localTheme = localStorage.getItem(key);
+
+    if(localTheme === value){
+      container.classList.add('dark');
+      themeButton.classList.add('active');
     }else{
-      suffix.classList.add('dark');
-      container.setAttribute('id', 'light')
-      suffix.src= `${light}`;
+      container.classList.remove('dark');
+      themeButton.classList.remove('active')
     }
-  })
+  }
+
+  darkMode('theme', 'true')
+
 }
